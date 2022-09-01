9f1d6ae0a0f717ec5483ed916bcf43963e98c440
TrungKien <kien.dangtrung.hust@gmail.com>
2022-09-02 01:16:48 +0200

    update

---
+++ python_general/decorator.py
@@ -0,0 +1,11 @@
+
+"""
+The main goal of this exercise is to understand decorators in python
+
+Question :
+https://www.hackerrank.com/domains/python/closures-and-decorators/difficulty/all/page/1
+
+Tutorial :
+
+https://www.datacamp.com/tutorial/decorators-python
+"""
