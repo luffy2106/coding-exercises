88152e9d45cd9e5be2fa388b666e8980e89b38e4
DANG Trung Kien <tkdang@assystem.com>
2022-09-02 17:39:12 +0200

    finish first exercices about decorator

--- python_general/decorator.py
+++ python_general/decorator.py
@@ -8,4 +8,51 @@ https://www.hackerrank.com/domains/python/closures-and-decorators/difficulty/all
 Tutorial :
 
 https://www.datacamp.com/tutorial/decorators-python
+
+
+Note :
+To see the difference between *args and **kwargs:
+https://www.programiz.com/python-programming/args-and-kwargs
+
+To understand closure and decorator and understand why decorator is useful (read section 8 and 9)
+
+http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
+
+
 """
+
+"""
+To solve the above question, make a list of the mobile numbers and pass it to a function that sorts the array in ascending order. 
+Make a decorator that standardizes the mobile numbers and apply it to the function.
+"""
+
+
+
+def wrapper(f):
+    def fun(l):
+        # complete the function
+        # l = "".join(l)
+        new_l = []
+        for e in l:
+            if len(e) > 10:
+                if e.startswith('+91'):
+                    e = e[3:]
+                elif e.startswith('91'):
+                    e = e[2:]
+                elif e.startswith('0'):
+                    e = e[1:]
+            e1 = e[:5]
+            e2 = e[5:]
+            e = "+91 "+e1 + " " + e2
+            new_l.append(e) 
+        ret = f(new_l)
+        return ret
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
