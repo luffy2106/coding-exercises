2636434975e0d1919154f8a233d1ff6fd2ba29b8
DANG Trung Kien <tkdang@assystem.com>
2024-03-28 19:29:32 +0100

    finish combination 3 but time limit error

--- algo_practice/recursive/templates/selection/combination_1.py
+++ algo_practice/recursive/templates/selection/combination_1.py
@@ -23,13 +23,78 @@ Constraints:
 
 1 <= n <= 20
 1 <= k <= n
+
+
+II. Solution
+
+Solution 1:
+1. Base case :
+If n = 0 , return empty list
+2. Recursion case :
+- Choose random number in the list, exclude it
+- recursive looking for all the permutation of list of k elements from the remain list
+
+Solution 2:
+
+
+Note:
+- permutation : A permutation is an arrangement of items in a specific order. selecting items "A, B, C" is different from selecting "C, B, A". 
+when we want permutation, use list.
+- combination : A combination is a selection of items without considering the order.  selecting items "A, B, C" is considered the same as selecting "C, B, A".
+when we want permutation, use set.
 """
 
 from typing import List
-
+import copy
+from itertools import combinations
 
 class Solution:
+
     def combine(self, n: int, k: int) -> List[List[int]]:
+        list_element =  list(range(1, n+1))
+        current_combination = set()
+        all_combinations = []
+        return self.recursive_combine(list_element, k, current_combination, all_combinations)
+
+
+    def recursive_combine(self, list_element: List[int], k: int, current_combination, all_combinations) -> List[List[int]]:
+        # Define base case:
+        if len(current_combination)==k:
+            if current_combination not in all_combinations: 
+                all_combinations.append(current_combination)
+        else:
+            # do recursive
+            if len(list_element) > 0:
+                for index in range(0, len(list_element)):
+                    remain_list_element = [list_element[i] for i in range(0, len(list_element)) if i!=index]
+                    temp_current_combination = copy.deepcopy(current_combination)
+                    temp_current_combination.add(list_element[index])
+                    self.recursive_combine(remain_list_element, k, temp_current_combination, all_combinations)
         
+        all_combinations = [list(e) for e in all_combinations]
+    
+        return all_combinations
+
+
         
-        return
+
+
+    
+
+def main():
+    # Write your main code logic here
+    n = 4
+    k = 2
+    print("This is the main function.")
+    x = Solution()
+    list_element = list(range(1,n+1))
+    print(x.combine(n,k))
+
+    # using built-in function
+    list_combination = [list(e) for e in list(combinations(list_element,k))]
+    print(list_combination)
+
+if __name__ == "__main__":
+    main()
+
+    
--- algo_practice/recursive/templates/selection/combination_2.py
+++ algo_practice/recursive/templates/selection/combination_2.py
@@ -1,33 +1,60 @@
 
 """
-Question : get all possible combination of string from a given string, take into account 
+Question : get all possible permutation and combination of string from a given string, take into account 
 
 
-Solution:
-- Get one elment from the string, use this element to combine with other elemment in the remain string
-
-
-
+Solution : 
+Get one elment from the string, use this element to combine with other elemment in the remain string
+- Base case: if the string is empty, then return None
+- Recursive case: Take one random element from string, try to take the permutation from the remain string. 
 
 """
+import copy
 
 s  = "abc"
 list_s = [e for e in s]
 remain = list_s
 pick = ""
+all =  []
 
-all =  set()
-def recursive(pick, remain):
+def get_permutation(pick, remain):
     if len(remain)==0:
         return
     else:
         for i in range(len(remain)):
             merge = pick + remain[i]
             if merge not in all:
-                all.add(merge)
+                all.append(merge)
             next_remain = [remain[e] for e in range(len(remain)) if e!=i]
-            recursive(merge, next_remain)
+            get_permutation(merge, next_remain)
 
-recursive(pick, remain)
+get_permutation(pick, remain)
 
 print(all)
+
+
+
+current_combination = set()
+all_combination = []
+
+
+def get_combination(list_s, current_combination:set, all_combination: list):
+    if len(list_s) == 0:
+        return None
+    else:
+        for index in range(0, len(list_s)):
+            remain_list = [list_s[e] for e in range(0, len(list_s)) if e!=index]
+            temp_current_combination = copy.deepcopy(current_combination)
+            temp_current_combination.add(list_s[index])
+            if temp_current_combination not in all_combination:
+                all_combination.append(temp_current_combination)
+            get_combination(remain_list, temp_current_combination, all_combination)
+    
+    all_combination = ["".join(list(e)) for e in all_combination]
+    return all_combination
+
+
+print(get_combination(list_s, current_combination, all_combination))
+
+
+
