f58ccd55385ffd1575b48ef77a0e737d1aa8e784
root <root@DESKTOP-NQDI2S4.localdomain>
2024-04-27 18:31:21 +0200

    update3

--- algo_practice/linked_list/SinglyLinkedList/Middle_of_linked_list.py
+++ algo_practice/linked_list/SinglyLinkedList/Middle_of_linked_list.py
@@ -1,5 +1,5 @@
 """
-1. Question 
+I. Question 
 https://leetcode.com/problems/middle-of-the-linked-list/description/
 
 Given the head of a singly linked list, return the middle node of the linked list.
@@ -15,10 +15,10 @@ Input: head = [1,2,3,4,5,6]
 Output: [4,5,6]
 Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
 
-2. Solution
-- count the total number of nodes in the list by traversing it once. 
-- Then, we calculate the index of the middle node.
-- we traverse the list again from the beginning to reach the middle node and return it
+II. Solution
+1. count the total number of nodes in the list by traversing it once. 
+2. Then, we calculate the index of the middle node.
+3. we traverse the list again from the beginning to reach the middle node and return it
 """
 
 from typing import Optional
