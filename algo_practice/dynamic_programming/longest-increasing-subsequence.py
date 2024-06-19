00ec8c364bee2bd3a4637dbde178cf37a375eac0
luffy2106 <tkdang@assystem.com>
2024-06-19 18:20:28 +0200

    finish longest-increasing-consequence

--- algo_practice/dynamic_programming/longest-increasing-subsequence.py
+++ algo_practice/dynamic_programming/longest-increasing-subsequence.py
@@ -24,44 +24,30 @@ Constraints:
 
 
 Solution :
+dp = [1] * n
+Let dp[i] represents the length of the longest increasing subsequence ending with nums[i]
+- dp[i] = max(dp[j] + 1, dp[i]) (in this way, the length of the longest increasing subsequen alway update in dp[i])
 
-Let L(i) be the length of the LIS(longest strictly increasing) ending at index i such that arr[i] is the last element of the LIS. Then, L(i) can be recursively written as: 
-- L(i) = 1 + max(list(L(j)) ) where 0 < j < i and arr[j] < arr[i]; or 
-- L(i) = 1, if no such j exists.
-Ex :
-Consider arr[] = {3, 10, 2, 11}
-- L[4] = max([L[i]  for i in [0,1,2,3] if arr[i] < arr[4]) + 1 
-- L[3] = max(L[1], L[2]) + 1 and 3 greater than 1,2
-- L[2] = max(L[1],L[0]) + 1 and 2 greater than 1
-- L[1] = max(L[0]) + 1 and 1 greater than 0
+result = max(dp)
+
+
 
 """
 from typing import List
 class Solution:
-    def lengthOfLIS_DP(self, n, dict_LIS, nums: List[int]) -> int:
-        if n == 0:
-            return 1
-        elif n >= 1:
-            list_LIS = []
-            for i in range(n-1):
-                LIS_i = 0
-                if nums[i] < nums[n-1]:
-                    if i in dict_LIS.keys():
-                        LIS_i = dict_LIS[i]
-                    else:
-                        LIS_i = self.lengthOfLIS_DP(i, dict_LIS, nums)
-                    list_LIS.append(LIS_i)      
-            
-            # lengthOfLIS_DP_
-            return max(list_LIS) + 1
 
-
-    
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dict_LIS = {}
-        result = self.lengthOfLIS_DP(n, dict_LIS, nums)
-        return result
+        if not nums:
+            return 0
+        # dp[i] represents the length of the longest increasing subsequence ending with nums[i]
+        dp  = [1] * len(nums)  
+        for i in range(1, len(dp)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+
+        return max(dp)
+
 
 
 nums = [10,9,2,5,3,7,101,18]
